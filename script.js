bee61b3e400b7d15cf6b6ae751694635b0ff1dce
NO_ob <15161159+NO-ob@users.noreply.github.com>
2019-12-30 00:12:32 +0000

    Implemented image laoding for all iamges in Gallery

---
+++ script.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name       Lanraragi Scroll View
+// @namespace  http://www.fuzetsu.com/MangaLoaderNSFW
+// @version    1.0
+// @description Makes all gallery images load on a single page
+// @noframes
+// @include *:3000/reader?id=*
+// ==/UserScript==
+
+function loadImages(){
+    var pageCount = document.getElementsByClassName("max-page")[0].innerText;
+    console.log( window.getComputedStyle(document.getElementById("img")));
+
+    for (var i = 1; i < pageCount; i++){
+        var img = loadImage(pages.pages[currentPage+i], canvasCallback);
+        img.className="reader-image";
+        if (localStorage.scaletoview === 'true'){
+            img.style="max-height: 90vh;";
+        }
+        document.getElementById('display').appendChild(img);
+    }
+}
+loadImages();
+
+
