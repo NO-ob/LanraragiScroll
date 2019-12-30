8ed9a066cfb31be258e4174238fc7bfd305d0a3d
NO_ob <15161159+NO-ob@users.noreply.github.com>
2019-12-30 21:06:26 +0000

    Made Toggleable

--- LanraragiScroll.user.js
+++ LanraragiScroll.user.js
@@ -5,20 +5,38 @@
 // @noframes
 // @include *:3000/reader?id=*
 // ==/UserScript==
-
-function loadImages(){
-    var pageCount = document.getElementsByClassName("max-page")[0].innerText;
-    console.log( window.getComputedStyle(document.getElementById("img")));
-
-    for (var i = 1; i < pageCount; i++){
-        var img = loadImage(pages.pages[currentPage+i], canvasCallback);
-        img.className="reader-image";
-        if (localStorage.scaletoview === 'true'){
-            img.style="max-height: 90vh;";
-        }
-        document.getElementById('display').appendChild(img);
-    }
-}
+displayToggleButton();
 loadImages();
 
+function displayToggleButton(){
+
+    var scrollerToggle = document.createElement("BUTTON");
+    scrollerToggle.innerText = "Toggle Scroller";
+    scrollerToggle.id = "scrollerToggle";
+    document.getElementById('i2').appendChild(scrollerToggle);
+    document.getElementById("scrollerToggle").addEventListener("click", toggle);
+
+}
+
+function loadImages(){
+    if (localStorage.scrollerEnabled == 1){
+        for (var i = 1; i < pageNumber; i++){
+            var img = loadImage(pages.pages[currentPage+i], canvasCallback);
+            img.className="reader-image";
+            if (localStorage.scaletoview === 'true'){
+                img.style="max-height: 90vh;";
+            }
+            document.getElementById('display').appendChild(img);
+        }
+    }
+}
+function toggle(){
+
+        if (localStorage.scrollerEnabled == 1){
+            localStorage.scrollerEnabled = 0;location.reload();
+        } else {
+            localStorage.scrollerEnabled = 1; console.log("scrollT"); loadImages();
+        }
+
+ }
 
